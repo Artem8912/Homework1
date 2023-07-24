8bde33c535aa4946d03e9d5d863500e0b6482e3d
Art <artemgoryunov89@gmail.com>
2023-07-24 19:46:21 +0400

    Программа для дней недели (версия1)

--- Exercise-15/Program.cs
+++ Exercise-15/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿void Dayoftheweek(int number)
+{
+    if(number == 6 || number == 7)
+    {
+           Console.WriteLine("Этот день - выходной"); 
+    }
+    else
+    {
+           Console.WriteLine("Этот день - не выходной");  
+    }
+}
+
+Console.WriteLine("Введите номер дня недели");
+int a = Convert.ToInt32(Console.ReadLine());
+Dayoftheweek(a);
+
+
