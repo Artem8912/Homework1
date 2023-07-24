e966ae85f7ee68af655e9eacdb36ec0db95b6b58
Art <artemgoryunov89@gmail.com>
2023-07-24 20:28:57 +0400

    Программа для определения третьей цифры

---
+++ Exercise-13a/Program.cs
@@ -0,0 +1,19 @@
+﻿int Thirddigit(int number)
+{
+    while (number > 999)
+    {
+        number = number / 10;
+    }
+    return (number % 100) % 10;
+}
+Console.WriteLine("Введите число");
+int a = Convert.ToInt32(Console.ReadLine());
+int result = Thirddigit(a);
+if (a < 100)
+{
+    Console.WriteLine("Третьей цифры нет");
+}
+else
+{
+    Console.WriteLine($"Третья цифра равна {result} ");
+}
