92ca160afa5c5639eea76fa4e77f5e71fa3de30e
Art <artemgoryunov89@gmail.com>
2023-07-24 15:06:49 +0400

    Программа для определения второй цифры числа

--- Exercise-10/Program.cs
+++ Exercise-10/Program.cs
@@ -1,2 +1,8 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Введите трёхзначное число");
+int a = Convert.ToInt32(Console.ReadLine());
+int Seconddigit(int number)
+{
+   return (a % 100)/10;
+}
+int result = Seconddigit(a);
+Console.WriteLine($"Вторая цифра числа равна {result}  ");
